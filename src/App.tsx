f159f1e2d70e9d7b23d6cef61043d8086b39985f
FumoRin <edu.dimasfaizsatria@gmail.com>
2024-09-09 20:20:16 +0700

    Fixed Sidebar commit compared to previous one

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import Message from "./Message";
-import ListGroup from "./components/ListGroup";
+import Sidebar, { SidebarItem } from "./components/sidebar";
+import {
+  HomeIcon,
+  BuildingLibraryIcon,
+  LifebuoyIcon,
+  Cog6ToothIcon,
+  ChatBubbleLeftRightIcon,
+  CalendarIcon,
+  DocumentIcon,
+} from "@heroicons/react/24/solid";
 
-function App () {
-  return <div><ListGroup /></div>
+function App() {
+  return (
+    <Sidebar>
+      <hr className="my-1" />
+      <SidebarItem icon={<HomeIcon />} text="Home" active={true} />
+      <SidebarItem icon={<BuildingLibraryIcon />} text="Materi" />
+      <SidebarItem icon={<DocumentIcon />} text="Tugas" />
+      <SidebarItem icon={<CalendarIcon />} text="Jadwal" />
+      <SidebarItem icon={<ChatBubbleLeftRightIcon />} text="Chat" />
+      <hr className="my-3" />
+      <SidebarItem icon={<LifebuoyIcon />} text="Bantuan" />
+      <SidebarItem icon={<Cog6ToothIcon />} text="Settings" />
+    </Sidebar>
+  );
 }
 
 export default App;
