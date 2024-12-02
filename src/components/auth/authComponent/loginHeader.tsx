a9051a056799596b4d40b929650c301aead4f5ed
Massa Triya Rahman Hadian <MassaRahaman7@gmail.com>
2024-12-02 13:17:09 +0700

    Svg logo

--- src/components/auth/authComponent/loginHeader.tsx
+++ src/components/auth/authComponent/loginHeader.tsx
@@ -20,7 +20,7 @@ export default function Header({
         <img
           alt=""
           className="h-14 w-14"
-          src="https://ik.imagekit.io/pibjyepn7p9/Lilac_Navy_Simple_Line_Business_Logo_CGktk8RHK.png?ik-sdk-version=javascript-1.4.3&updatedAt=1649962071315"
+          src="D:\SMKN 1 CIMAHI\SIJA\PKK2\MySTMPBDG_-_Frontend\src\assets\1.svg"
         />
       </div>
       <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
